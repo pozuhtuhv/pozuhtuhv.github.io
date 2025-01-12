ee4b947dcb861dfb2756e5f7a4807ebae7f86ed7
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2025-01-12 10:25:49 +0900

    (edit) auto_tag.py

--- auto_tag.py
+++ auto_tag.py
@@ -4,23 +4,26 @@ import os
 
 res = requests.get('https://pozuhtuhv.github.io/tag/')
 
-# BeautifulSoup 객체 생성
 soup = BeautifulSoup(res.text, 'html.parser')
 
-# 'body > main > section.tags > ul > li > a' 선택자에 해당하는 모든 <a> 태그 찾기
+# 모든 <a> 태그 찾기
 tags = soup.select('body > main > section.tags > ul > li > a')
+
+parent_folder = 'tag'
+if not os.path.exists(parent_folder):
+    os.makedirs(parent_folder)
 
 for tag in tags:
     tag_name = tag.get_text().split(' (')[0]  # 각 <a> 태그의 텍스트 출력
 
-        # 폴더 생성 (없으면 생성)
-    if not os.path.exists(tag_name):
+    #  tag 폴더 안에 폴더 생성
+    if not os.path.exists(parent_folder, tag_name):
         os.makedirs(tag_name)
 
     # index.html 내용 작성
     index_html_content = f'''---
 layout: tag
-tag: '{tag_name}
+tag: {tag_name}
 ---
 '''
 
@@ -28,4 +31,4 @@ tag: '{tag_name}
     with open(os.path.join(tag_name, 'index.html'), 'w', encoding='utf-8') as f:
         f.write(index_html_content)
 
-    print(f'폴더 "{tag_name}"와 index.html 파일이 생성되었습니다.')
+    print(f'폴더 "{tag_name}"와 index.html 파일이 생성')
