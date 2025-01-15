636b1da8d44095c9c15a4a2ec981305eb56fd44e
Hyeonseok-Cho <pozuhtuhv@gmail.com>
2025-01-16 03:18:24 +0900

    (edit) giscus.css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,22 @@
-// D-day 계산 및 텍스트 적용
 document.addEventListener("DOMContentLoaded", function() {
-    var now = new Date();
-    var dDayDate = new Date('2023-05-01');
-    var timeDiff = now.getTime() - dDayDate.getTime();
-    var daysRemaining = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    
-    // #dDay 요소에 텍스트 설정 및 스타일 적용
-    var dDayElement = document.getElementById('dDay');
-    dDayElement.textContent = 'D+' + daysRemaining;
-    dDayElement.style.textAlign = 'center';
-    dDayElement.style.paddingTop = '6px';
-  });
+  // 현재 페이지의 경로 가져오기
+  var currentPath = window.location.pathname;
+
+  // 경로가 "/" 또는 비어 있는 경우에만 실행
+  if (currentPath === '/' || currentPath === '') {
+      var now = new Date();
+      var dDayDate = new Date('2023-05-01');
+      var timeDiff = now.getTime() - dDayDate.getTime();
+      var daysRemaining = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+      // #dDay 요소에 텍스트 설정 및 스타일 적용
+      var dDayElement = document.getElementById('dDay');
+      if (dDayElement) {
+          dDayElement.textContent = 'D+' + daysRemaining;
+          dDayElement.style.textAlign = 'center';
+          dDayElement.style.paddingTop = '6px';
+      } else {
+          console.warn("id='dDay' 요소가 존재하지 않습니다.");
+      }
+  }
+});
