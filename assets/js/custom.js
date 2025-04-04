29f9c7ee8fced794e1654fe3d26c01bfc588ada5
pozuhtuhv <pozuhtuhv@gmail.com>
2025-04-04 18:20:10 +0900

    (edit) post_layouts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const codeBlocks = document.querySelectorAll('pre');
-  
-    codeBlocks.forEach(block => {
-      const button = document.createElement('button');
-      button.innerText = 'Copy';
-      button.className = 'copy-button';
-  
-      button.addEventListener('click', () => {
-        const code = block.querySelector('code').innerText;
-        navigator.clipboard.writeText(code).then(() => {
-          button.innerText = 'Copied!';
-          setTimeout(() => {
-            button.innerText = 'Copy';
-          }, 2000);
-        }).catch(err => {
-          console.error('Failed to copy text: ', err);
-        });
-      });
-  
-      block.appendChild(button);
-    });
-  });
-
 // 버튼 요소 가져오기
 var topButton = document.getElementById("topButton");
 
