5cfbbfc5e18ed48560752150ad9e3af764e56205
pozuhtuhv <pozuhtuhv@gmail.com>
2025-04-18 00:11:40 +0900

    (edit) css_optimizer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var topButton = document.getElementById("topButton");
 
-  // 버튼이 존재할 때만 이벤트 연결
   if (topButton) {
-    // 스크롤 시 버튼 표시/숨김 처리
     window.addEventListener("scroll", function () {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topButton.style.display = "block";
@@ -12,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    // 버튼 클릭 시 상단으로 이동
     topButton.addEventListener("click", function () {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
